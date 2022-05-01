8ad22925599e8f107c5fd8c1ab66ba9f4a1e9c16
Harun <hkolos1@etf.unsa.ba>
2022-05-01 15:51:08 +0200

    Like,Dislike .png

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import chevronUp from './chevron-up.svg';
+import chevronDown from './chevron-down.svg';
+import liked from './liked.png';
+import notLiked from './not-liked.png';
+import disliked from './disliked.png';
+import notDisliked from './not-disliked.png';
+
+export {chevronUp, chevronDown, liked, notLiked, disliked, notDisliked};
